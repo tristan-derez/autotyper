9a612bf97ef526354410049be3de3c221e6b4daf
Tristan Derez <92602528+dreyzu@users.noreply.github.com>
2022-08-15 16:21:41 +0200

    move verification line and add indent

--- script.py
+++ script.py
@@ -10,33 +10,33 @@ colorama.init(autoreset=True)
 def repeat_action(times, f, *args):
     for i in range(times): f(*args)
 
-if __name__ == __main__ : # make sure script is main and not imported before executing script
 def type(string):
   pyautogui.write(string, interval = 0.5)
   sleep(randint(1, 2))
   pyautogui.hotkey('enter')
   sleep(randint(0, 3))
 
-print(Fore.YELLOW + "What do you want to type? 🙂")
-string = input(Fore.CYAN + "")
-print(Fore.YELLOW + "Well... weird thing to type but ok! 😷")
-print(Fore.YELLOW + "How many times?")
-num = input(Fore.CYAN + "")
-input(Fore.YELLOW + "Press Enter key then put your cursor where you want to type, you will have a delay of 5 seconds ⏰")
-sleep(5)
-print(Fore.YELLOW + "Typing", Fore.CYAN + string, Fore.YELLOW + "a total of",Fore.CYAN + num, Fore.YELLOW + "times 🤣")
+if __name__ == __main__ : # make sure script is main and not imported before executing script
+  print(Fore.YELLOW + "What do you want to type? 🙂")
+  string = input(Fore.CYAN + "")
+  print(Fore.YELLOW + "Well... weird thing to type but ok! 😷")
+  print(Fore.YELLOW + "How many times?")
+  num = input(Fore.CYAN + "")
+  input(Fore.YELLOW + "Press Enter key then put your cursor where you want to type, you will have a delay of 5 seconds ⏰")
+  sleep(5)
+  print(Fore.YELLOW + "Typing", Fore.CYAN + string, Fore.YELLOW + "a total of",Fore.CYAN + num, Fore.YELLOW + "times 🤣")
 
-start = int(timer())
-repeat_action(int(num), type, string)
-end = int(timer())
+  start = int(timer())
+  repeat_action(int(num), type, string)
+  end = int(timer())
 
-timers_seconds = end - start
-timers_minutes = int(timers_seconds / 60)
+  timers_seconds = end - start
+  timers_minutes = int(timers_seconds / 60)
 
-if timers_seconds <= 59:
-  print(Fore.GREEN + "Done in", timers_seconds,Fore.GREEN + "seconds! 😎")
-elif timers_minutes <= 1:
-  print(Fore.GREEN + "Done in", timers_minutes,Fore.GREEN + "minute! 🤓")
-else: 
-  print(Fore.GREEN + "Done in", timers_minutes,Fore.GREEN + "minutes! 😴")
+  if timers_seconds <= 59:
+    print(Fore.GREEN + "Done in", timers_seconds,Fore.GREEN + "seconds! 😎")
+  elif timers_minutes <= 1:
+    print(Fore.GREEN + "Done in", timers_minutes,Fore.GREEN + "minute! 🤓")
+  else: 
+    print(Fore.GREEN + "Done in", timers_minutes,Fore.GREEN + "minutes! 😴")
 
